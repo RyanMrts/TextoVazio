87fdf1718dd8a81e5127dc9f2d7b769b52466dc0
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-01 18:01:37 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+class Program {
+static void Main(string[] args) {
+Console.Write("Digite um texto: ");
+string texto = Console.ReadLine()!;
+
+if (string.IsNullOrWhiteSpace(texto)) {
+Console.WriteLine("false");
+} else {
+            Console.WriteLine("true");
+        }
+    }
+}
+
